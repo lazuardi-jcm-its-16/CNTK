c31c98704f67fe795bd4d1eeeed5030e2bbe1497
David Pisani <dapisani@microsoft.com>
2017-04-25 11:44:10 -0700

    08 to 8

--- bindings/python/cntk/learners/__init__.py
+++ bindings/python/cntk/learners/__init__.py
@@ -740,11 +740,11 @@ def fsadagrad(parameters, lr, momentum, unit_gain=default_unit_gain_value(),
 
 @typemap
 def adam(parameters, lr, momentum, unit_gain=default_unit_gain_value(),
-         variance_momentum=momentum_as_time_constant_schedule(720000), epsilon=1e-08,
+         variance_momentum=momentum_as_time_constant_schedule(720000), epsilon=1e-8,
          l1_regularization_weight=0.0, l2_regularization_weight=0.0,
          gaussian_noise_injection_std_dev=0.0, gradient_clipping_threshold_per_sample=np.inf,
          gradient_clipping_with_truncation=True, use_mean_gradient=default_use_mean_gradient_value()):
-    '''adam(parameters, lr, momentum, unit_gain=default_unit_gain_value(), variance_momentum=momentum_as_time_constant_schedule(720000), epsilon=1e-08, l1_regularization_weight=0, l2_regularization_weight=0, gaussian_noise_injection_std_dev=0, gradient_clipping_threshold_per_sample=np.inf, gradient_clipping_with_truncation=True)
+    '''adam(parameters, lr, momentum, unit_gain=default_unit_gain_value(), variance_momentum=momentum_as_time_constant_schedule(720000), epsilon=1e-8, l1_regularization_weight=0, l2_regularization_weight=0, gaussian_noise_injection_std_dev=0, gradient_clipping_threshold_per_sample=np.inf, gradient_clipping_with_truncation=True)
     Creates an Adam learner instance to learn the parameters. See [1] for more
     information.
 
@@ -759,7 +759,7 @@ def adam(parameters, lr, momentum, unit_gain=default_unit_gain_value(),
         variance_momentum (output of :func:`momentum_schedule` or :func:`momentum_as_time_constant_schedule`): variance momentum schedule. Defaults
          to ``momentum_as_time_constant_schedule(720000)``.
         epsilon (float, optional): numerical stability constant,
-         defaults to 1e-08
+         defaults to 1e-8
         l1_regularization_weight (float, optional): the L1 regularization weight per sample,
          defaults to 0.0
         l2_regularization_weight (float, optional): the L2 regularization weight per sample,
